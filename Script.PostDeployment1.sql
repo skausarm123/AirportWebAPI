760f0d182fb4d5b633ee4f7f3ddd934f8061ef69
skausarm123 <146542791+skausarm123@users.noreply.github.com>
2023-11-21 15:38:55 +0530

    Add files via upload

---
+++ Script.PostDeployment1.sql
@@ -0,0 +1,10 @@
+﻿if not exists(select 5 from dbo.[Airport])
+begin
+insert into dbo.[Airport] 
+values('LGW',1,'Departure and Arrival')
+insert into dbo.[Airport] 
+values('PMI',2,'Arrival Only')
+insert into dbo.[Airport] 
+values('LAX',3,'Arrival Only')
+
+end
